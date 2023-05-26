4094667c943887854390a400a95667ccef3661c9
Muhammad Irfan Zaidan <irfanzaidan1703@gmail.com>
2023-05-26 16:30:45 +0700

    membuat file

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('#inputForm').submit(function(e) {
+      e.preventDefault();
+  
+      var Angka = $('#Angka').val();
+      var Text = $('#Text').val();
+  
+      $.ajax({
+        url: 'formB.php',
+        type: 'POST',
+        data: {
+          Angka: Angka,
+          Text: Text
+        },
+        dataType: 'json',
+        success: function(response) {
+          displayTable(response);
+        },
+        error: function(xhr, status, error) {
+          console.log(error);
+        }
+      });
+    });
+  
+    function displayTable(data) {
+      var tbody = $('#table tbody');
+      tbody.empty();
+  
+      $.each(data, function(index, item) {
+        var row = '<tr>' +
+                    '<td>' + index + '</td>' +
+                    '<td>' + item + '</td>' +
+                  '</tr>';
+        tbody.append(row);
+      });
+  
+      $('#table').slideDown();
+    }
+  });  
